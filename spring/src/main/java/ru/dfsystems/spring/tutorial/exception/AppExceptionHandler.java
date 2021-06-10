225791e42ef5f4c94e00a550f0df21bf7cf27a22
cuprumtan <cuprumtan@gmail.com>
2021-06-10 10:10:39 +0500

    Добавлено логирование

--- spring/src/main/java/ru/dfsystems/spring/tutorial/exception/AppExceptionHandler.java
+++ spring/src/main/java/ru/dfsystems/spring/tutorial/exception/AppExceptionHandler.java
@@ -1,5 +1,7 @@
 package ru.dfsystems.spring.tutorial.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class AppExceptionHandler {
 
+    private static Logger logger = LoggerFactory.getLogger(AppExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handler(Exception e){
         e.printStackTrace();
+        logger.info("ОШИБКА: " + e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
